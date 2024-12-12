25e21696d38643afc5581b895857a7fc1de1d1f4
AMOREM <dev@amorem.com>
2024-12-12 16:30:19 +0100

    ImageGeneration Configuration component

---
+++ src/app/(dashboard)/image-generation/_components/Configurations.tsx
@@ -0,0 +1,286 @@
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Slider } from "@/components/ui/slider";
+import { Textarea } from "@/components/ui/textarea";
+
+const formSchema = z.object({
+  model: z.string({
+    required_error: "Model is required",
+  }),
+  prompt: z.string({
+    required_error: "Prompt is required",
+  }),
+  guidance: z.number({
+    required_error: "Guidance scale is required",
+  }),
+  num_outputs: z
+    .number()
+    .min(1, {
+      message: "Number of outputs should be at least 1",
+    })
+    .max(4, {
+      message: "Number of outputs do not exceed 4",
+    }),
+  aspect_ratio: z.string({
+    required_error: "Aspect ration is required",
+  }),
+  output_format: z.enum(["webp", "jpg", "png"], {
+    required_error: "Output format is required",
+  }),
+  output_quality: z
+    .number()
+    .min(1, {
+      message: "quality should be at least 1",
+    })
+    .max(100, { message: "quality does not exceed 100" }),
+
+  num_inference_steps: z
+    .number()
+    .min(1, {
+      message: "inference should be at least 1",
+    })
+    .max(50, { message: "inference does not exceed 50" }),
+});
+
+export function Configurations() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      model: "black-forest-labs/flux-dev",
+      prompt: "",
+      guidance: 3.5,
+      num_outputs: 1,
+      output_format: "jpg",
+      aspect_ratio: "1:1",
+      output_quality: 80,
+      num_inference_steps: 28,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <fieldset className="grid gap-6 p-4 bg-background rounded-lg border">
+          <legend className="text-sm -ml-1 px-1 font-medium">Settings</legend>
+          <FormField
+            control={form.control}
+            name="model"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Model</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a model" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="black-forest-labs/flux-dev">
+                      Flux Dev
+                    </SelectItem>
+                    <SelectItem value="black-forest-labs/flux-schnell">
+                      Flux Schnell
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex gap-4 w-full">
+            <FormField
+              control={form.control}
+              name="aspect_ratio"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Aspect Ratio</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a model" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="1:1">1:1</SelectItem>
+                      <SelectItem value="16:9">16:9</SelectItem>
+                      <SelectItem value="9:16">9:16</SelectItem>
+                      <SelectItem value="21:9">21:9</SelectItem>
+                      <SelectItem value="9:21">9:21</SelectItem>
+                      <SelectItem value="4:5">4:5</SelectItem>
+                      <SelectItem value="5:4">5:4</SelectItem>
+                      <SelectItem value="4:3">4:3</SelectItem>
+                      <SelectItem value="3:4">3:4</SelectItem>
+                      <SelectItem value="2:3">2:3</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="num_outputs"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Number of Images</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      min={1}
+                      max={4}
+                      {...field}
+                      onChange={(event) => field.onChange(+event.target.value)}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name="guidance"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex items-center justify-between">
+                  <div>Guidance</div>
+                  <span>{field.value}</span>
+                </FormLabel>
+                <FormControl>
+                  <Slider
+                    defaultValue={[field.value]}
+                    min={0}
+                    max={10}
+                    step={0.5}
+                    onValueChange={(value) => field.onChange(value[0])}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="num_inference_steps"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex items-center justify-between">
+                  <div>Inference</div>
+                  <span>{field.value}</span>
+                </FormLabel>
+                <FormControl>
+                  <Slider
+                    defaultValue={[field.value]}
+                    min={1}
+                    max={50}
+                    step={1}
+                    onValueChange={(value) => field.onChange(value[0])}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="output_quality"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex items-center justify-between">
+                  <div>Quality</div>
+                  <span>{field.value}</span>
+                </FormLabel>
+                <FormControl>
+                  <Slider
+                    defaultValue={[field.value]}
+                    min={50}
+                    max={100}
+                    step={1}
+                    onValueChange={(value) => field.onChange(value[0])}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="output_format"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Output Format</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select an output format" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="jpg">JPG</SelectItem>
+                    <SelectItem value="png">PNG</SelectItem>
+                    <SelectItem value="webp">WEBP</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="prompt"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Prompt</FormLabel>
+                <FormControl>
+                  <Textarea {...field} rows={6} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="font-medium">
+            Generate
+          </Button>
+        </fieldset>
+      </form>
+    </Form>
+  );
+}
--- src/app/(dashboard)/image-generation/page.tsx
+++ src/app/(dashboard)/image-generation/page.tsx
@@ -1,3 +1,12 @@
+import { Configurations } from "./_components/Configurations";
+
 export default function ImageGenerationPage() {
-  return <div>Image Generation</div>;
+  return (
+    <section className="container mx-auto grid gap-4 grid-cols-3 overflow-hidden">
+      <Configurations />
+      <div className="col-span-2 p-4 rounded-xl flex items-center justify-center">
+        Output Images
+      </div>
+    </section>
+  );
 }
